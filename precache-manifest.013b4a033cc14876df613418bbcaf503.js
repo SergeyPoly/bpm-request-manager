7fefcac150afc3073d5fd4e2c1b1a58c010038da
Sergey Polischuk <spoly82@gmail.com>
2021-01-20 05:09:49 +0200

    Updates

---
+++ precache-manifest.013b4a033cc14876df613418bbcaf503.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "165ddd5a890cb83b006e01e55cd40b9b",
+    "url": "/bpm-request-manager/index.html"
+  },
+  {
+    "revision": "d8400141d0f524e8ba4d",
+    "url": "/bpm-request-manager/static/css/2.cbe6aa73.chunk.css"
+  },
+  {
+    "revision": "f11d40261a8bd09bfc27",
+    "url": "/bpm-request-manager/static/css/main.9e967c0b.chunk.css"
+  },
+  {
+    "revision": "d8400141d0f524e8ba4d",
+    "url": "/bpm-request-manager/static/js/2.6eaa2e40.chunk.js"
+  },
+  {
+    "revision": "4f80036cb2bb336303569dea20591006",
+    "url": "/bpm-request-manager/static/js/2.6eaa2e40.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36961b7b4057ad949646",
+    "url": "/bpm-request-manager/static/js/3.690797a1.chunk.js"
+  },
+  {
+    "revision": "f11d40261a8bd09bfc27",
+    "url": "/bpm-request-manager/static/js/main.836d7bb5.chunk.js"
+  },
+  {
+    "revision": "f7d638fcc3a07af9b59b",
+    "url": "/bpm-request-manager/static/js/runtime-main.93464802.js"
+  }
+]);
